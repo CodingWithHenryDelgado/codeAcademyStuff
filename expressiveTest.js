315f12c6361f324b66b38853f62136da57680553
Henry_DelGlitch <CodingWHenry@gmail.com>
2021-05-27 00:04:40 -0400

    Added Deep Equal to the assert expressive test.

--- expressiveTest.js
+++ expressiveTest.js
@@ -20,4 +20,18 @@ describe('-', () => {
       assert.equal(result, expected);
     });
   });
+
+  describe('+', () => {
+    it('returns the sum of two values', () => {
+      // Setup
+          let expected = {a: 3, b: 4, result: 7};
+          let sum = {a: 3, b: 4};
+  
+      // Exercise
+          sum.result = sum.a + sum.b;
+  
+      // Verify
+      assert.deepEqual(sum, expected);
+    });
+  });
   
