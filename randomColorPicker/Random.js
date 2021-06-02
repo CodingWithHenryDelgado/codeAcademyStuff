2f654a55b70f83cd0b8432196cc82b2c966abcf4
Henry_DelGlitch <CodingWHenry@gmail.com>
2021-06-02 12:16:30 -0400

    Added solution to randomColorPicker

--- randomColorPicker/Random.js
+++ randomColorPicker/Random.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Button } from './Button';
 
 class Random extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      color: [10, 250, 3]
+    }
+    this.handleClick = this.handleClick.bind(this);
+  }
   componentDidMount() {
     this.applyColor();
   }
@@ -32,12 +40,20 @@ class Random extends React.Component {
     return random;
   }
 
+  handleClick() {
+    this.setState({ color: this.chooseColor() })
+  }
+
   render() {
     return (
       <div>
         <h1 className={this.isLight() ? 'white' : 'black'}>
-
+  Your color is {this.formatColor(this.state.color)}
         </h1>
+        <Button 
+          light={this.isLight()}
+          onClick={this.handleClick}
+        />
       </div>
     );
   }
