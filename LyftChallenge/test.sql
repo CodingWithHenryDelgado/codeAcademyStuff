9d10248b0591734df1bdef9c2fdf94f985375f99
Henry_DelGlitch <CodingWHenry@gmail.com>
2021-08-23 19:31:26 -0400

    Completed lyft challenge

---
+++ LyftChallenge/test.sql
@@ -0,0 +1,32 @@
+-- SELECT * FROM trips;
+ 
+-- SELECT * FROM riders;
+ 
+-- SELECT * FROM cars;
+
+-- SELECT *
+-- FROM trips
+-- LEFT JOIN riders 
+--   ON trips.rider_id = riders.id;
+
+-- SELECT *
+-- FROM trips
+-- JOIN cars
+--   ON trips.car_id = cars.id
+
+-- SELECT *
+-- FROM riders
+-- UNION
+-- SELECT *
+-- FROM riders2;
+
+-- SELECT AVG(cost)
+-- FROM trips;
+
+-- SELECT *
+-- FROM riders
+-- WHERE total_trips < 500;
+
+SELECT *
+FROM cars
+WHERE status = 'active';
