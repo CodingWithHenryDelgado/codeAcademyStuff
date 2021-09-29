e3a3d2bef67ad0557b8cf099164d0374c34f58f3
Henry_DelGlitch <CodingWHenry@gmail.com>
2021-09-29 10:59:14 -0400

    Removed folder

--- oauth-playlist/config/keys.js
+++
@@ -1,8 +0,0 @@
-//Add this file to .gitignore
-
-module.exports = {
-    google: {
-        clientID: '682650259876-5kg5sclte16l1udph2c7ngq4r475u02e.apps.googleusercontent.com',
-        clientSecret: 'KiKUI5TpwkbX_jboQG1KDbV6'
-    }
-}
